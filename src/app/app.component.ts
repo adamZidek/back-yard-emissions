7c71590789a530c162ea6b79b640b820d847e16c
Adam Zidek <35748357+adamZidek@users.noreply.github.com>
2023-09-24 21:21:38 -0400

    Get leaflet installed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Map, map, tileLayer } from 'leaflet';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'back-yard-emissions';
+
+  public map!: Map;
+
+  constructor() {
+    
+  }
+
+  ngOnInit() {
+    
+  }
+
+  ngAfterViewInit() {
+    this.map = map('map');
+    this.map.setView([51.505, -0.09], 13);
+    tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(this.map);
+  }
 }
+
+
